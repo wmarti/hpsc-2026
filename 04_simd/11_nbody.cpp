7b1e7f903500463f3a180c25068af7c24c94dc98
Will Martin <wmarti@bu.edu>
2026-05-07 10:00:31 +0900

    [AVX-512] Homework 3

--- 04_simd/11_nbody.cpp
+++ 04_simd/11_nbody.cpp
@@ -1,26 +1,30 @@
 #include <cstdio>
 #include <cstdlib>
-#include <cmath>
+#include <x86intrin.h>
 
 int main() {
   const int N = 16;
-  float x[N], y[N], m[N], fx[N], fy[N];
+  alignas(64) float x[N], y[N], m[N], fx[N], fy[N];
   for(int i=0; i<N; i++) {
     x[i] = drand48();
     y[i] = drand48();
     m[i] = drand48();
     fx[i] = fy[i] = 0;
   }
+  __m512 xvec = _mm512_load_ps(x);
+  __m512 yvec = _mm512_load_ps(y);
+  __m512 mvec = _mm512_load_ps(m);
+  __m512 jvec = _mm512_set_ps(15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0);
   for(int i=0; i<N; i++) {
-    for(int j=0; j<N; j++) {
-      if(i != j) {
-        float rx = x[i] - x[j];
-        float ry = y[i] - y[j];
-        float r = std::sqrt(rx * rx + ry * ry);
-        fx[i] -= rx * m[j] / (r * r * r);
-        fy[i] -= ry * m[j] / (r * r * r);
-      }
-    }
+    __m512 rx = _mm512_sub_ps(_mm512_set1_ps(x[i]), xvec);
+    __m512 ry = _mm512_sub_ps(_mm512_set1_ps(y[i]), yvec);
+    __m512 r2 = _mm512_fmadd_ps(rx, rx, _mm512_mul_ps(ry, ry));
+    __m512 inv = _mm512_rsqrt14_ps(r2);
+    __mmask16 mask = _mm512_cmp_ps_mask(jvec, _mm512_set1_ps((float)i), _MM_CMPINT_NE);
+    inv = _mm512_mask_blend_ps(mask, _mm512_setzero_ps(), inv);
+    __m512 inv3 = _mm512_mul_ps(_mm512_mul_ps(inv, inv), inv);
+    fx[i] = -_mm512_reduce_add_ps(_mm512_mul_ps(_mm512_mul_ps(rx, mvec), inv3));
+    fy[i] = -_mm512_reduce_add_ps(_mm512_mul_ps(_mm512_mul_ps(ry, mvec), inv3));
     printf("%d %g %g\n",i,fx[i],fy[i]);
   }
 }
